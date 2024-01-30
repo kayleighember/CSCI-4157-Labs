9ef024e8449276ffe2695a96fc288b4219d143d8
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2024-01-29 19:18:02 -0500

    Created branch for VertexBuffer

--- Lec03GraphicsObject/Main.cpp
+++ Lec03GraphicsObject/Main.cpp
@@ -198,11 +198,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // The camera's z points down the -ve world z
     viewOriginal[2] = glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
 
-    float left = -10.0f;
-    float right = 10.0f;
+    float left = -50.0f;
+    float right = 50.0f;
+    float bottom = -50.0f;
+    float top = 50.0f;
     left *= aspectRatio;
     right *= aspectRatio;
-    glm::mat4 projection = glm::ortho(left, right, -10.0f, 10.0f, -1.0f, 1.0f);
+    glm::mat4 projection = glm::ortho(left, right, bottom, top, -1.0f, 1.0f);
 
     std::shared_ptr<VertexBuffer> buffer = std::make_shared<VertexBuffer>(6);
     buffer->AddVertexData(6, -5.0f, 5.0f, 0.0f, 1.0f, 0.0f, 0.0f);
