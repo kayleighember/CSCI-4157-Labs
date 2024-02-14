5f192730534fdf9ac85bd2cedc61d39a7d9c91a3
Kayleigh <postke@etsu.edu>
2024-02-14 15:10:38 -0500

    Lab 4 Checkpoint 3 cleared

--- KPInteractiveGraphicsS24/Texture.cpp
+++ KPInteractiveGraphicsS24/Texture.cpp
@@ -2,8 +2,39 @@
 
 Texture::Texture() {
 	textureData = nullptr;
+	isLoadedFromFile = false;
+	glGenTextures(1, &textureId);
+}
+
+Texture::~Texture() {
+	Cleanup();
 }
 
 unsigned char* Texture::GetTextureData() {
 	return textureData;
+}
+
+void Texture::SetTextureData(unsigned int count, unsigned char* data) {
+	Cleanup();
+	textureData = new unsigned char[count];
+	memcpy(data, textureData, count);
+	isLoadedFromFile = false;
+}
+
+unsigned int Texture::GetTextureId() {
+	return textureId;
+}
+
+bool Texture::IsLoadedFromFile() {
+	return isLoadedFromFile;
+}
+
+void Texture::Cleanup() {
+	if (textureData == nullptr) {
+		return;
+	}
+	if (!isLoadedFromFile) {
+		delete[] textureData;
+		textureData = nullptr;
+	}
 }
--- KPInteractiveGraphicsS24/Texture.h
+++ KPInteractiveGraphicsS24/Texture.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "BaseObject.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 class Texture : public BaseObject
 {
 private:
 	unsigned char* textureData;
+	bool isLoadedFromFile;
+	unsigned int textureId;
 public:
 	Texture();
-	~Texture() = default;
+	~Texture();
 	unsigned char* GetTextureData();
+	void SetTextureData(unsigned int count, unsigned char* data);
+	bool IsLoadedFromFile();
+	unsigned int GetTextureId();
+	void Cleanup();
 };
 
